ea8e3c063a94070073020c19f71b040d63937dbb
Jelmer Vernooĳ <jelmer@jelmer.uk>
2026-03-22 14:52:40 +0000

    Add support for files-included

--- debian-copyright/src/lossless.rs
+++ debian-copyright/src/lossless.rs
@@ -101,6 +101,8 @@ const HEADER_FIELD_ORDER: &[&str] = &[
     "Upstream-Name",
     "Upstream-Contact",
     "Source",
+    "Files-Excluded",
+    "Files-Included",
     "Disclaimer",
     "Comment",
     "License",
@@ -588,6 +590,19 @@ impl Header {
     pub fn set_files_excluded(&mut self, files: &[&str]) {
         self.0
             .set_with_field_order("Files-Excluded", &files.join("\n"), HEADER_FIELD_ORDER);
+    }
+
+    /// List of files re-included after exclusion by `Files-Excluded`
+    pub fn files_included(&self) -> Option<Vec<String>> {
+        self.0
+            .get("Files-Included")
+            .map(|x| x.split('\n').map(|x| x.to_string()).collect::<Vec<_>>())
+    }
+
+    /// Set included files
+    pub fn set_files_included(&mut self, files: &[&str]) {
+        self.0
+            .set_with_field_order("Files-Included", &files.join("\n"), HEADER_FIELD_ORDER);
     }
 
     /// Fix the the header paragraph
@@ -2082,6 +2097,135 @@ License: MIT
         assert_eq!(
             vec!["*", "src/*", "src/foo/*", "debian/*"],
             files[0].files()
+        );
+    }
+
+    #[test]
+    fn test_files_included_none() {
+        let s = r#"Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: example
+
+Files: *
+License: GPL-3+
+Copyright: 2019 John Doe
+"#;
+        let copyright = s.parse::<super::Copyright>().expect("failed to parse");
+        assert_eq!(copyright.header().unwrap().files_included(), None);
+    }
+
+    #[test]
+    fn test_files_included_single() {
+        let s = r#"Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: example
+Files-Included: src/important.c
+
+Files: *
+License: GPL-3+
+Copyright: 2019 John Doe
+"#;
+        let copyright = s.parse::<super::Copyright>().expect("failed to parse");
+        assert_eq!(
+            copyright.header().unwrap().files_included(),
+            Some(vec!["src/important.c".to_string()])
+        );
+    }
+
+    #[test]
+    fn test_files_included_multiple() {
+        let s = r#"Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: example
+Files-Included: src/important.c
+ src/also-important.c
+ docs/*
+
+Files: *
+License: GPL-3+
+Copyright: 2019 John Doe
+"#;
+        let copyright = s.parse::<super::Copyright>().expect("failed to parse");
+        assert_eq!(
+            copyright.header().unwrap().files_included(),
+            Some(vec![
+                "src/important.c".to_string(),
+                "src/also-important.c".to_string(),
+                "docs/*".to_string(),
+            ])
+        );
+    }
+
+    #[test]
+    fn test_set_files_included() {
+        let s = r#"Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: example
+
+Files: *
+License: GPL-3+
+Copyright: 2019 John Doe
+"#;
+        let copyright = s.parse::<super::Copyright>().expect("failed to parse");
+        let mut header = copyright.header().unwrap();
+        header.set_files_included(&["src/important.c", "docs/*"]);
+        assert_eq!(
+            header.files_included(),
+            Some(vec![
+                "src/important.c".to_string(),
+                "docs/*".to_string(),
+            ])
+        );
+    }
+
+    #[test]
+    fn test_files_excluded_none() {
+        let s = r#"Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: example
+
+Files: *
+License: GPL-3+
+Copyright: 2019 John Doe
+"#;
+        let copyright = s.parse::<super::Copyright>().expect("failed to parse");
+        assert_eq!(copyright.header().unwrap().files_excluded(), None);
+    }
+
+    #[test]
+    fn test_files_excluded_multiple() {
+        let s = r#"Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: example
+Files-Excluded: vendor/*
+ .github/*
+
+Files: *
+License: GPL-3+
+Copyright: 2019 John Doe
+"#;
+        let copyright = s.parse::<super::Copyright>().expect("failed to parse");
+        assert_eq!(
+            copyright.header().unwrap().files_excluded(),
+            Some(vec![
+                "vendor/*".to_string(),
+                ".github/*".to_string(),
+            ])
+        );
+    }
+
+    #[test]
+    fn test_set_files_excluded() {
+        let s = r#"Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Upstream-Name: example
+
+Files: *
+License: GPL-3+
+Copyright: 2019 John Doe
+"#;
+        let copyright = s.parse::<super::Copyright>().expect("failed to parse");
+        let mut header = copyright.header().unwrap();
+        header.set_files_excluded(&["vendor/*", ".github/*"]);
+        assert_eq!(
+            header.files_excluded(),
+            Some(vec![
+                "vendor/*".to_string(),
+                ".github/*".to_string(),
+            ])
         );
     }
 
